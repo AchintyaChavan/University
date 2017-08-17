9125046942e255c9ad410d0cf00a4172ea7c2c1b
AC <AC@192.168.1.3>
2017-08-17 17:05:44 +1000

    Starting Search Implementation

--- Assignments/a1-3702-43213889/a1-3702-43213889.py
+++ Assignments/a1-3702-43213889/a1-3702-43213889.py
@@ -13,9 +13,21 @@ outputFile = "H:\\Documents\\Achintya\\UQ\\Engineering\\5th Year\\Sem 2 2017\\CO
 import argparse
 import pdb
 
+import numpy as np
+
 import re
 import road
 import search
+
+def string_split(string):
+    
+    for i, c in enumerate(string):
+        
+        if not c.isdigit():
+            
+            break
+    
+    return i
 
 def file_read(filename):
     
@@ -27,24 +39,42 @@ def file_read(filename):
     
     return array
 
-def environment_read(array):
+def junction_read(array):
     
-    graph = {}
+    junction = {}
     
     for r in array:
         
-        temp = r.split(";")        
-        
+        temp = r.split(";")
+
+        road = temp[0].strip()
+        start = temp[1].strip()
+        end = temp[2].strip()        
         length = int(temp[3].strip())
         nLots = int(temp[4].strip())
         
-        for i in range(nLots):       
+        if start not in junction:
+                
+            junction[start] = [[road, start, end, length, nLots]]
+            
+        else:
+            
+            junction[start].append([road, start, end, length, nLots])
+            
+        if end not in junction:
+            
+            junction[end] = [[road, start, end, length, nLots]]
+            
+        else:
+            
+            junction[end].append([road, start, end, length, nLots])      
         
-            graph[str(i+1) + temp[0].strip()] = road.Node(i + 1, 
-                                   temp[1].strip(), temp[2].strip(), 
-                                   length, nLots)
-        
-    return graph
+#         for i in range(nLots):       
+#         
+#             graph[str(i+1) + temp[0].strip()] = road.Node(i + 1, 
+#                                    temp[1].strip(), temp[2].strip(), 
+#                                    length, nLots)
+    return junction
 
 def queries_read(array):
     
@@ -54,7 +84,18 @@ def queries_read(array):
         
         temp = q.split(";")
         
-        queries.append([temp[0].strip(), temp[1].strip()])
+        start = temp[0].strip()
+        end = temp[1].strip()
+        
+        i1, i2 = string_split(start), string_split(end)
+                
+        n1 = start[i1:]
+        a1 = int(start[:i1])
+        
+        n2 = end[i2:]
+        a2 = int(end[:i2])
+        
+        queries.append(road.Query(n1, a1, n2, a2))
         
     return queries       
        
@@ -69,17 +110,20 @@ def main():
     f1 = file_read(environmentFile)
     f2 = file_read(queryFile)
     
-    graph = environment_read(f1)
+    junction = junction_read(f1)
     queries = queries_read(f2)
     
+#     for j, i in junction.iteritems(): print(j,i)
+#     
+#     print(queries)
     
-    
-    for q in queries:
-        cost, path = search.breadth_first_search(graph, q[0], q[1])
-         
-        print(cost, path)
+#     for q in queries:
+#         cost, path = search.breadth_first_search(junction, q)
+#      
+#           
+#         print(cost, path)
 
-    print(graph)
+    cost, path = search.breadth_first_search(junction, queries[0])
 
     return 0;
 
--- Assignments/a1-3702-43213889/search.py
+++ Assignments/a1-3702-43213889/search.py
@@ -4,7 +4,12 @@ Created on 16 Aug. 2017
 @author: AC
 '''
 
+import math
 import road
+
+def address_length(length, plotSize, addrNo):
+    
+    return 2 * Length/plotSize * math.ceil(addrNo / 2.) - 1
 
 def adjacent(graph, current):
     
@@ -13,14 +18,26 @@ def adjacent(graph, current):
     return
 
 
-def breadth_first_search(graph, start, end):
+def breadth_first_search(graph, query):
     
     cost = []
-    queue = {}
+    priorityQueue = {}
+
+    start = query.name1
+    a1 = query.address1
+    goal = query.name2
+    a2 = query.address2
+
+    for key, val in graph.iteritems():
+        
+        for v in val:
+                    
+            if v[0] == start:
+            
+                priorityQueue[v[1]] = val
+        
+        
     
-    queue[start] = graph[start]
-    addr = start
+    print(priorityQueue)
     
-    
-    
-    return cost, queue
+    return cost, priorityQueue
