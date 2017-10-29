21c9cf59315b394da058a7a31c91010a5e857fa6
AC <AC@192.168.1.3>
2017-10-29 14:01:26 +1000

    Implemented for loops in main and added argument checking

--- Assignments/a3-3702-43213889/a3-3702-43213889.py
+++ Assignments/a3-3702-43213889/a3-3702-43213889.py
@@ -4,17 +4,33 @@ Created on 18 Oct. 2017
 @author: AC
 '''
 
-# import numpy as np
+import argparse
 import os
 import Simulator as Sim
 
-
 inputfile = "bronze1.txt"
-outfile = "output1.txt"
+outputfile = "output1.txt"
+
+# The default number of simulations to run. 
+DEFAULT_NUM_SIMULATIONS = 1
+
+# Whether to re-create the solver for every simulation. 
+RECREATE_SOLVER = False;
 
 def main():
 
     epsilon = 4e-5   #Convergence Value
+    totalProfit = 0
+    
+    # The number of simulations to run. */
+    numSimulations = DEFAULT_NUM_SIMULATIONS; 
+    
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("inputFile", help = "list of roads and info")
+#     parser.add_argument("outputFile", help = "initial and final locations")
+#     args = parser.parse_args()
+        
+            
     filename = os.path.join(os.getcwd(), inputfile)
  
     #Initialise ProblemSpec Class
@@ -24,17 +40,48 @@ def main():
 
     #Initialise Solver Class
     solver = Sim.MySolver.MySolver(problem, epsilon)
-    solver.doOfflineComputation('value')  #Perform base value iteration
-    
+        
     #Initialise Simulator Class with ProblemSpec and Solver classes
     simulator = Sim.Simulator()
-    simulator.constructor(problem)
-    simulator.setVerbose(True)
-    simulator.simulateStep(solver, 16)
-                
-#     S0 = tuple([key for key in problem.getInitialFunds().values()])
+    simulator.constructor(solver.problem)
+    """
+    ####Change this to True for it to display fortnightly results - Default is False
+    """
+    simulator.setVerbose(False)
+    
+     #Perform base value iteration
+    if RECREATE_SOLVER == False:
         
-#     simulator.saveStep(os.path.join(os.getcwd(), outfile))
+        print("Solver Initialised")
+        solver.doOfflineComputation("value") 
+    
+    #Run simulator this many times
+    for simNo in range(numSimulations):
+        
+        print("Simulation Run ", simNo + 1)                
+        simulator.reset()
+        
+        if RECREATE_SOLVER == True:
+            
+            print("Solver Reinitialised")
+            solver = Sim.MySolver.MySolver(problem, epsilon)
+            solver.doOfflineComputation("value")
+                
+        print("-----------------------------------------------")
+        
+        #Perform simulations over the entire fortnight
+        for i in range(solver.problem.getNumFortnights()):
+                       
+            simulator.simulateStep(solver, solver.problem.getNumFortnights() - (i + 1))
+        
+        totalProfit += simulator.getTotalProfit()
+        print("-----------------------------------------------")
+        
+        
+    simulator.saveStep(os.path.join(os.getcwd(), outputfile))
+#     print("-----------------------------------------------")
+    print("Summary statistics from " + str(numSimulations) + " runs")
+    print("Overall profit: " + str(totalProfit))          
     
     return 0
 
